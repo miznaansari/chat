449cf129ddcd456ff4e23957fbc1582f703780e8
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-08-03 01:36:11 +0530

    Configure independent node_modules and Prisma Client for nextaichat

--- nextaichat/next.config.mjs
+++ nextaichat/next.config.mjs
@@ -1,6 +1,11 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    root: path.resolve("./"),
+  },
 };
 
 export default nextConfig;
